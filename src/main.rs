f5e9f94671ff8eb6002549f0d1fef46d340377d1
Mitchell Bruckner <mbruckner807@gmail.com>
2019-06-09 16:19:08 -0500

    some database helper functions

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,37 @@
 
 #[macro_use] extern crate rocket;
 
+use rocket::State;
+use serde::{Serialize, Deserialize};
+use serde::de::DeserializeOwned;
+use sled::Db;
+use bincode::{serialize, deserialize};
+
+fn get<E: DeserializeOwned>(db: &Db, key: &str) -> Result<Option<E>, Box<dyn std::error::Error>> {
+    Ok(match db.get(key)? {
+        Some(x) => Some(deserialize(&x)?),
+        None => None
+    })
+}
+
+fn set<E: Serialize>(db: &Db, key: &str, value: &E) -> Result<(), Box<dyn std::error::Error>> {
+    db.set(key, serialize(value)?)?;
+    Ok(())
+}
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index(base: State<Db>) -> Result<String, Box<dyn std::error::Error>> {
+    let mut visits = get::<usize>(&base, "visits")?.unwrap_or(0);
+    visits += 1;
+    set(&base, "visits", &visits)?;
+    Ok(format!("Hello! You are visitor number {}", visits))
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    let database = Db::start_default("database").unwrap();
+    rocket::ignite()
+        .mount("/", routes![index])
+        .manage(database)
+        .launch();
+    print!("ENDING");
 }
