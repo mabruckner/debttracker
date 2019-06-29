bdbfbc05d18a08dfde9b7c2f83812e639dbb89bf
Mitchell Bruckner <mbruckner807@gmail.com>
2019-06-28 23:05:36 -0500

    key type

--- src/main.rs
+++ src/main.rs
@@ -8,23 +8,28 @@ use serde::de::DeserializeOwned;
 use sled::Db;
 use bincode::{serialize, deserialize};
 
-fn get<E: DeserializeOwned>(db: &Db, key: &str) -> Result<Option<E>, Box<dyn std::error::Error>> {
-    Ok(match db.get(key)? {
+#[derive(Serialize, Deserialize)]
+enum DbKey {
+    User(String)
+}
+
+fn get<E: DeserializeOwned>(db: &Db, key: &DbKey) -> Result<Option<E>, Box<dyn std::error::Error>> {
+    Ok(match db.get(serialize(key)?)? {
         Some(x) => Some(deserialize(&x)?),
         None => None
     })
 }
 
-fn set<E: Serialize>(db: &Db, key: &str, value: &E) -> Result<(), Box<dyn std::error::Error>> {
-    db.set(key, serialize(value)?)?;
+fn set<E: Serialize>(db: &Db, key: &DbKey, value: &E) -> Result<(), Box<dyn std::error::Error>> {
+    db.set(serialize(key)?, serialize(value)?)?;
     Ok(())
 }
 
 #[get("/")]
 fn index(base: State<Db>) -> Result<String, Box<dyn std::error::Error>> {
-    let mut visits = get::<usize>(&base, "visits")?.unwrap_or(0);
+    let mut visits = get::<usize>(&base, &DbKey::User("HELLO".into()))?.unwrap_or(0);
     visits += 1;
-    set(&base, "visits", &visits)?;
+    set(&base, &DbKey::User("HELLO".into()), &visits)?;
     Ok(format!("Hello! You are visitor number {}", visits))
 }
 
